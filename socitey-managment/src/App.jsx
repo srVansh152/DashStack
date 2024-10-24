6f565622569cecad3179b7be2f9c85fe7f83d69d
Abhay kahar <abhay.kahar@swiftrut.com>
2024-10-25 05:15:34 +0530

    editproflie

--- socitey-managment/src/App.jsx
+++ socitey-managment/src/App.jsx
@@ -3,6 +3,9 @@ import RegistrationForm from './components/RegistrationPage'
 import Login from './components/Login'
 import ForgetPassword from './components/ForgetPassword'
 import OtpVerification from './components/OtpVerification'
+import DashboardLayout from './Components/DashboardLayout'
+import ResetPassword from './Components/ResetPassword'
+import Editprofile from './Components/Editprofile'
 
 
 function App() {
@@ -14,6 +17,9 @@ function App() {
           <Route path='/login' element={<Login/>}/>
           <Route path='/password' element={<ForgetPassword/>}/>
           <Route path='/otp' element={<OtpVerification/>}/>
+          <Route path='/dashboard' element={<DashboardLayout/>}/>
+          <Route path='/reset' element={<ResetPassword/>}/>
+          <Route path='/editprofile' element={<Editprofile/>}/>
         </Routes>
       </BrowserRouter>
     
