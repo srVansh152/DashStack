18313655acadc5650d338dd439b708ef144c6500
srvansh152 <vansh.dobariya@swiftrut.com>
2024-10-26 00:51:16 +0530

    Update Society.js

--- Server-Society-System-Management/models/Society.js
+++ Server-Society-System-Management/models/Society.js
@@ -4,6 +4,16 @@ const societySchema = new mongoose.Schema({
   name: { type: String, required: true },
   location: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
+  residents: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Resident'
+    }
+  ],
+  occupiedUnits: {
+    type: Number,
+    default: 0
+  }
 });
 
 module.exports = mongoose.model('Society', societySchema);
