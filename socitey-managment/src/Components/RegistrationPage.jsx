3836e95a33e9739fdf4a7693a363f147a468b475
Abhay kahar <abhay.kahar@swiftrut.com>
2024-10-25 05:35:04 +0530

    register

--- socitey-managment/src/Components/RegistrationPage.jsx
+++ socitey-managment/src/Components/RegistrationPage.jsx
@@ -195,7 +195,7 @@ export default function RegistrationPage() {
             </div>
             <button
               type="submit"
-              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 border p-3"
+              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-orange-600 hover:bg-orange-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 border p-3"
             >
               Register
             </button>
@@ -275,7 +275,7 @@ export default function RegistrationPage() {
                       id="state"
                       name="state"
                       required
-                      placeholder="Enter Name"
+                     placeholder="Enter Name"
                       className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50 border p-3"
                     />
                   </div>
