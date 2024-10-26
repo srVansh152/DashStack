de4e1e9fae6b7981bb7d70688f23d411b98e8561
srvansh152 <vansh.dobariya@swiftrut.com>
2024-10-26 06:36:40 +0530

    Update Society.js

--- Server-Society-System-Management/models/Society.js
+++ Server-Society-System-Management/models/Society.js
@@ -1,8 +1,30 @@
 const mongoose = require('mongoose');
 
 const societySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  location: { type: String, required: true },
+  societyname: {
+    type: String,
+    require: true
+  },
+  societyaddress: {
+    type: String,
+    require: true
+  },
+  Country: {
+    type: String,
+    require: true
+  },
+  State: {
+    type: String,
+    require: true
+  },
+  City: {
+    type: String,
+    require: true
+  },
+  Zipcode: {
+    type: Number,
+    require: true
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
