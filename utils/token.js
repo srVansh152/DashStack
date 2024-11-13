1c311669eeb6b31395c0a9b4813c4658c61f0188
Trupesh Savaliya <trupesh.savaliya@swiftrut.com>
2024-11-13 21:19:09 +0530

    delete extra codes from main branch

--- utils/token.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (user) => {
-  return jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: '1h',
-  });
-};
-
-module.exports = { generateToken };
