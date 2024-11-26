4e7944f5b7086e66dde2c2fd6bb3c4babc9eebce
srvansh152 <vansh.dobariya@swiftrut.com>
2024-11-27 04:49:48 +0530

    delet develop folders

--- backend/routes/society.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { createSociety, getSocieties } = require('../controllers/societyController');
-const router = express.Router();
-
-router.post('/create', createSociety);
-router.get('/', getSocieties);
-
-module.exports = router;
