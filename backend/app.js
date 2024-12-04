f2e87293b483bc4c129d331bd2811ef0f510741b
srvansh152 <vansh.dobariya@swiftrut.com>
2024-12-04 22:50:59 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors({
     origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true
 }));
 app.use(morgan('dev'));
