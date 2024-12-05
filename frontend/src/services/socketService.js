1e5aad1b12730ecccacceaa6a9e86a6f8b6fb83e
jenishbabariya22 <jenishbabariya58@gmail.com>
2024-12-05 22:11:09 +0530

    new residence

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -1,64 +1,64 @@
-import io from 'socket.io-client';
+// import io from 'socket.io-client';
 
-class SocketService {
-  constructor() {
-    this.socket = null;
-    this.baseURL = 'https://socitey-management-system-server.onrender.com';
-  }
+// class SocketService {
+//   constructor() {
+//     this.socket = null;
+//     this.baseURL = 'https://socitey-management-system-server.onrender.com';
+//   }
 
-  connect() {
-    if (!this.socket) {
-      this.socket = io(this.baseURL, {
-        transports: ['websocket'],
-        reconnection: true,
-        reconnectionAttempts: 5,
-        reconnectionDelay: 1000,
-        auth: {
-          token: localStorage.getItem('token')
-        }
-      });
+//   connect() {
+//     if (!this.socket) {
+//       this.socket = io(this.baseURL, {
+//         transports: ['websocket'],
+//         reconnection: true,
+//         reconnectionAttempts: 5,
+//         reconnectionDelay: 1000,
+//         auth: {
+//           token: localStorage.getItem('token')
+//         }
+//       });
 
-      this.socket.on('connect', () => {
-        console.log('Socket connected successfully');
-        const userId = localStorage.getItem('userId');
-        if (userId) {
-          this.socket.emit('userConnected', userId);
-        }
-      });
+//       this.socket.on('connect', () => {
+//         console.log('Socket connected successfully');
+//         const userId = localStorage.getItem('userId');
+//         if (userId) {
+//           this.socket.emit('userConnected', userId);
+//         }
+//       });
 
-      this.setupDefaultListeners();
-    }
-    return this.socket;
-  }
+//       this.setupDefaultListeners();
+//     }
+//     return this.socket;
+//   }
 
-  setupDefaultListeners() {
-    if (!this.socket) return;
+//   setupDefaultListeners() {
+//     if (!this.socket) return;
 
-    this.socket.on('connect_error', (error) => {
-      console.error('Socket connection error:', error);
-      setTimeout(() => {
-        this.socket.connect();
-      }, 1000);
-    });
+//     this.socket.on('connect_error', (error) => {
+//       console.error('Socket connection error:', error);
+//       setTimeout(() => {
+//         this.socket.connect();
+//       }, 1000);
+//     });
 
-    this.socket.on('disconnect', (reason) => {
-      console.log('Socket disconnected:', reason);
-      if (reason === 'io server disconnect') {
-        this.socket.connect();
-      }
-    });
+//     this.socket.on('disconnect', (reason) => {
+//       console.log('Socket disconnected:', reason);
+//       if (reason === 'io server disconnect') {
+//         this.socket.connect();
+//       }
+//     });
 
-    this.socket.on('ping', () => {
-      this.socket.emit('pong');
-    });
-  }
+//     this.socket.on('ping', () => {
+//       this.socket.emit('pong');
+//     });
+//   }
 
-  disconnect() {
-    if (this.socket) {
-      this.socket.disconnect();
-      this.socket = null;
-    }
-  }
-}
+//   disconnect() {
+//     if (this.socket) {
+//       this.socket.disconnect();
+//       this.socket = null;
+//     }
+//   }
+// }
 
-export default new SocketService();
+// export default new SocketService();
