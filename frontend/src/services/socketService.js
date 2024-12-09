8c36b14de10eee68f5bc4c1161c23cbcfe17ec3a
srvansh152 <vansh.dobariya@swiftrut.com>
2024-12-09 23:45:07 +0530

    changes in my code

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -3,7 +3,7 @@ import io from 'socket.io-client';
 class SocketService {
   constructor() {
     this.socket = null;
-    this.baseURL = 'http://localhost:5000';
+    this.baseURL = 'https://dashstack-90hs.onrender.com';
   }
 
   connect() {
